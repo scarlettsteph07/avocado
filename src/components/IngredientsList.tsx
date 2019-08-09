88107184d08019e1d3b5f268bf9a0ddfd2d12ca1
Scarlett Tejada <scarlettsteph07@gmail.com>
2019-08-09 10:46:06 -0700

    Add IngredientsList component

---
+++ src/components/IngredientsList.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import _ from 'lodash'
+
+import styled from '../styled'
+
+interface Props {
+  ingredients: Object,
+}
+
+export const IngredientsList = ({ ingredients }: Props) => {
+  return (
+    <IngredientsListStyles className="ingredients">
+      <div className="ingredients__list">
+        {
+          _.map(
+            ingredients,
+            (ingredient, i) => <div key={i} className="ingredients__list__item">{ingredient.name}</div>
+          )
+        }
+      </div>
+    </IngredientsListStyles>
+  )
+}
+
+const IngredientsListStyles = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-flow: column;
+
+  .ingredients {
+    &__list {
+      width: 100%;
+      height: auto;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      align-items: center;
+      align-content: space-evenly;
+      &__item {
+        display: flex;
+        justify-content: center;
+      }
+    }
+  }
+`
