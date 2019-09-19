d0e763232635bb3b6d4598e4859056e50cad6b01
Scarlett Tejada <scarlettsteph07@gmail.com>
2019-09-19 14:03:41 -0700

    Update App test, add ThemeProvider

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'emotion-theming'
+
 import App from './App'
+import { theme } from './theme'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>,
+    div,
+  )
   ReactDOM.unmountComponentAtNode(div)
 })
