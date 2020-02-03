0aa0d8e97004b54d7c9eb0013d7d8531f71cc623
Adam Saegebarth <adam.saegebarth@gmail.com>
2020-02-02 20:39:27 -0800

    fix formatting

--- src/actions/errors.ts
+++ src/actions/errors.ts
@@ -1,13 +1,13 @@
 export const SET_ERROR = 'SET_ERROR'
 
 export interface SetErrorAction {
-    type: typeof SET_ERROR
-    errorName: string
+  type: typeof SET_ERROR
+  errorName: string
 }
 
 export type ErrorActions = SetErrorAction
 
 export const setError = (errorName: string): SetErrorAction => ({
-    type: SET_ERROR,
-    errorName,
+  type: SET_ERROR,
+  errorName,
 })
--- src/reducers/error.ts
+++ src/reducers/error.ts
@@ -1,18 +1,15 @@
-import {
-    SET_ERROR,
-    ErrorActions
-} from '../actions/errors'
+import { SET_ERROR, ErrorActions } from '../actions/errors'
 
 import { ErrorState } from '../types/store'
 
 export const error = (
-    state = '',
-    action: ErrorActions,
+  state = '',
+  action: ErrorActions,
 ): ErrorState => {
-    switch (action.type) {
-        case SET_ERROR:
-            return action.errorName
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case SET_ERROR:
+      return action.errorName
+    default:
+      return state
+  }
 }
