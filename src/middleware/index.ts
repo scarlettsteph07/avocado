9f98642c69ee92a83a0ae240a2d21d955f692681
Scarlett Saegebarth <scarlettsteph07@gmail.com>
2020-02-04 16:20:07 -0800

    Revert "WIP"
    This reverts commit d843ffdc52d0bb9fa3a8ef1ecf2104596c8a78f6.

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,8 +2,4 @@ import { applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
-import { logger } from './logger'
-
-export const middleware = composeWithDevTools(
-  applyMiddleware(thunk, logger),
-)
+export const middleware = composeWithDevTools(applyMiddleware(thunk))
